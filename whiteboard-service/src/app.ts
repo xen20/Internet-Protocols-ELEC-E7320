7c1379cb53c58ee35d2fddf84f75add32c8e07cb
navid ahmad <navid.ahmad@aalto.fi>
2023-02-20 19:44:18 +0200

    added temorary views

--- whiteboard-service/src/app.ts
+++ whiteboard-service/src/app.ts
@@ -1,17 +1,21 @@
 import exp from "constants";
 import express, {Express, Response, Request} from "express";
+import path from "path";
 
 const app : Express = express();
 
 // Express config
 app.set("port", process.env.PORT || 3000);
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
 
 
 /**
  * Primary app routes.
  */
 app.get('/', (req : Request, res : Response) => {
-    res.send("Hello from server");
+    console.log(`dirname: ${__dirname}`);
+    res.render( "index" );
 });
 
 export default app;
