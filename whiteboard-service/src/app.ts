7393b7d0e6d44968729820dd1b045e42db6b24b4
navid ahmad <navid.ahmad@aalto.fi>
2023-02-20 20:17:53 +0200

    added route for create session

--- whiteboard-service/src/app.ts
+++ whiteboard-service/src/app.ts
@@ -1,4 +1,3 @@
-import exp from "constants";
 import express, {Express, Response, Request} from "express";
 import path from "path";
 
@@ -6,16 +5,18 @@ const app : Express = express();
 
 // Express config
 app.set("port", process.env.PORT || 3000);
-app.set( "views", path.join( __dirname, "views" ) );
+app.set("views", path.join(__dirname, "../views"));
 app.set( "view engine", "ejs" );
-
 
 /**
  * Primary app routes.
- */
+*/
 app.get('/', (req : Request, res : Response) => {
-    console.log(`dirname: ${__dirname}`);
+    console.log(`dirname: ${path.join( __dirname, "views" )}`);
     res.render( "index" );
 });
 
+app.get('/create', (req : Request, res : Response) => {
+    res.send('Going to create session');
+})
 export default app;
